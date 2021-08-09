b42811a8f270bdc34b89a9f898a43ceb08bdd419
Samuel Thng <samuel@thng.sg>
2021-08-09 19:24:30 +0800

    Updated: Attempt 2

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,10 +4,10 @@ type Config = {
   readonly base_url: string;
   readonly site_title: string;
   readonly site_description: string;
-  readonly site_keywords: { keyword: string }[];
+  readonly site_keywords: { keyword: string[] };
   readonly posts_per_page: number;
   readonly twitter_account: string;
   readonly github_account: string;
 };
 
-export default config as Config;
+export default config as unknown as Config;
